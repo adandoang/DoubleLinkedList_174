caacb9f26d68f52cd0d6b1c9d945a0568ca48362
adandoang <akhdanjzif@gmail.com>
2023-05-26 11:28:20 +0700

    masi sama

--- Doublelinkedlist_174/Doublelinkedlist_174.cpp
+++ Doublelinkedlist_174/Doublelinkedlist_174.cpp
@@ -53,6 +53,7 @@ void DoubleLinkedList::addNode() {
 		START = newNode; //step 6
 		return;
 	}
+
 	/*insterting a node between two nodes in the list*/
 	Node* current = START; //step 1.a
 	Node* previous = NULL; //step 1.b
@@ -61,10 +62,15 @@ void DoubleLinkedList::addNode() {
 		previous = current; //1.d
 		current = current->next; //1.e
 	}
+
 	if (current->next != NULL && nim == current->next->noMhs) {
 		cout << "\nDuplicate roll number not allowed" << endl;
 		return;
 	}
 
 	newNode->next = current->next; //step 4
+	newNode->prev = current; //step 5
+	if (current->next != NULL)
+		current->next->prev = newNode; //step 6
+	current->next = newNode; //step 7
 }
